6c1e77a8c403d170a9e201993699fef226e0e5fc
raul <raul24786@gmail.com>
2021-06-09 17:03:47 +0200

    1

---
+++ 03_personal-info.py
@@ -0,0 +1,3 @@
+firstname = input("Cual es tu nombre?")
+lastname= input("Cual es tu apellido?")
+print("Tu nombre es:", firstname, lastname)
---
+++ test.py
@@ -0,0 +1,4 @@
+import time
+
+print ("ola mundo")
+time.sleep(15)
